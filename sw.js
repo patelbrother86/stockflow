67b4f20e3d7dffcbf72e88e7fb1e7f90267682de
patelbrother86 <patelbrother86@gmail.com>
2026-01-11 12:30:06 +0530

    Added Icon, fixed progressive app, Dedicated Scanner support

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'stockflow-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
